1f15ca2671c8a8871230c9e66b2cb1c32f38a677
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2022-04-01 13:53:32 -0700

    api automatico de los ingredientes, consumir api con feth y mostrarlo la pagina web

--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
-const API_URL = 'http://localhost:3000/api/ingredientes';
+const API_URL = 'http://localhost:3000/api/ingredientes/';
+
+fetch(API_URL)
+.then(response => response.json())
+.then(data => {
+  
+    let element = document.getElementById('app')
+    const data1 = data.map(user => {
+     return `<li>${user.nombre}</li>
+     <li>${user.descripcion}</li>`
+    });
+    element.innerHTML = `${data1}`
+
+})
+.catch(err=>console.log(err))
--- routes/ingredientes.js
+++ routes/ingredientes.js
@@ -5,10 +5,36 @@ const Ingredientes = require('../models/ingredientes');
 const router = Router();
 
 router.get('/',async(req, res)=> {
-
   const data = await Ingredientes.find();
   res.status(200).json( data )
   });
+
+const apiNombreIngredintes = async()=>{
+  const data = await Ingredientes.find();
+  const newARR = data.map(e=>{ return e.nombre})
+  newARR.forEach(e =>{
+   
+      const separar = e.split(' ');
+      if (separar.length>1){
+         unir = separar.join('-');
+         router.get(`/${unir}`,async(req, res)=> {
+          const data = await Ingredientes.find({nombre: e});
+          res.status(200).json( data )
+          });
+
+      }else{
+
+      router.get(`/${e}`,async(req, res)=> {
+        const data = await Ingredientes.find({nombre: e});
+        res.status(200).json( data )
+        });
+      }
+   
+  })
+}
+apiNombreIngredintes()
+
+
 
 const tipo = ['frutas','vejetales','fish']
 tipo.forEach(e =>{
